5f33583db05b9546399157cd8916ea9b8396a65d
Wasiq <nwasiq@gmail.com>
2020-06-14 13:31:35 +0500

    Calling a simple API

---
+++ block_io_lib/block_io_lib/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Web;
+
+namespace block_io_lib
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string apiUrl = String.Format("https://jsonplaceholder.typicode.com/todos/1");
+            WebRequest requestObj = WebRequest.Create(apiUrl);
+            requestObj.Method = "GET";
+            HttpWebResponse responseObjGet = null;
+            responseObjGet = (HttpWebResponse)requestObj.GetResponse();
+            string result = null;
+            using (Stream stream = responseObjGet.GetResponseStream())
+            {
+                StreamReader sr = new StreamReader(stream);
+                result = sr.ReadToEnd();
+                sr.Close();
+            }
+            Console.WriteLine("This is the result:"+  result);
+
+        }
+    }
+}
