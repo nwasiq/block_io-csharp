eed259915e7c1317b6b8887188560ef1b0332fb1
Wasiq <nwasiq@gmail.com>
2020-07-04 08:03:13 +0500

    Update Program.cs

--- block_io_lib/Program.cs
+++ block_io_lib/Program.cs
@@ -13,14 +13,8 @@ namespace block_io_lib
              * TESTS
              */
 
-            string WifTestStr = "L1cq4uDmSKMiViT4DuR8jqJv8AiiSZ9VeJr82yau5nfVQYaAgDdr";
-            string PassphraseStr = "deadbeef";
-            string DataToSignStr = "e76f0f78b7e7474f04cc14ad1343e4cc28f450399a79457d1240511a054afd63";
             string api_key = "27d6-fc3a-1606-e6d4";
-            string pin = "Was1qWas1q";
-            // Key tests
-
-            Program.RunKeyTests(WifTestStr, PassphraseStr, DataToSignStr);
+            string pin = "";
 
             // Withdraw
 
@@ -37,36 +31,6 @@ namespace block_io_lib
             //
             //Console.WriteLine(test.SweepFromAddress(sweepArgs).Data);
 
-        }
-
-        public static void RunKeyTests(string WifTestStr, string PassphraseStr, string DataToSignStr) 
-        {
-            // Key From WIF
-
-            Key testKey = new Key().ExtractKeyFromPassphrase(PassphraseStr);
-            string privateKey = testKey.ToHex();
-            string publicKey = testKey.PubKey.ToHex();
-            string signature = Helper.SignInputs(testKey, DataToSignStr, publicKey);
-
-            Console.WriteLine("Private Key From Passphrase: " + privateKey);
-            Console.WriteLine("Public Key From Passphrase: " + publicKey);
-            Console.WriteLine("Signature From Passphrase: " + signature);
-
-            /////////////////////////////////
-
-            Console.WriteLine("");
-            Console.WriteLine("");
-
-            // Key From Passphrase
-
-            testKey = new Key().FromWif(WifTestStr);
-            privateKey = testKey.ToHex();
-            publicKey = testKey.PubKey.ToHex();
-            signature = Helper.SignInputs(testKey, DataToSignStr, publicKey);
-
-            Console.WriteLine("Private Key From WIF: " + privateKey);
-            Console.WriteLine("Public Key From WIF: " + publicKey);
-            Console.WriteLine("Signature From WIF: " + signature);
         }
     }
 }
